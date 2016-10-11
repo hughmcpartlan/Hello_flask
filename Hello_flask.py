2a2fba98ac9f66431b3ddc7515a6453fdb31e638
Hughbert <hugh.mcpartlan1@gmail.com>
2016-10-11 14:23:23 +0100

    new page 'sometging'

--- Hello_flask.py
+++ Hello_flask.py
@@ -24,6 +24,12 @@ players = [{
 def hello_world():
     return render_template("index.html", players=players)
 
+nationalities = ['Italian', 'Welsh', 'Italian']
+
+@app.route('/something')
+def something_else():
+    return render_template("index.html", nationalities=nationalities)
+
 
 if __name__ == '__main__':
     app.run()
